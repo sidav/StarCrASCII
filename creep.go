c90c530cddc0949a46a65f7dee90bd21fc864e80
Unknown <sidav94@gmail.com>
2019-06-29 01:01:25 +0300

    Beautifies the code.

--- creep.go
+++ creep.go
@@ -1,6 +1,10 @@
 package main
 
-const CREEP_SPREAD_PERIOD = 100 // in ticks
+const (
+	CREEP_SPREAD_PERIOD         = 150 // in ticks
+	CREEP_APPEAR_PROBABILITY    = 2   // more == less likely; 1 == always
+	CREEP_DISAPPEAR_PROBABILITY = 3   // same
+)
 
 func isTimeToSpreadCreep() bool {
 	return CURRENT_TICK%CREEP_SPREAD_PERIOD == 0
@@ -47,12 +51,12 @@ func (g *gameMap) renewCreepSpread(creepers *[]*pawn) {
 				if isInDistance && curTile.creep {
 					continue nextCell
 				}
-				if isInDistance && (g.isCellNeighbouringACreep(x, y) || creeper.IsCloseupToCoords(x, y)) && (x+y+CURRENT_TICK/CREEP_SPREAD_PERIOD) % 2 == 0 {
+				if isInDistance && (g.isCellNeighbouringACreep(x, y) || creeper.IsCloseupToCoords(x, y)) && (x+y+CURRENT_TICK/CREEP_SPREAD_PERIOD)%CREEP_APPEAR_PROBABILITY == 0 {
 					newCreepTiles = append(newCreepTiles, curTile)
 					continue nextCell
 				}
 			}
-			if !isInDistance && curTile.creep && g.getNumberOfCreepInRect(x-1, y-1, 3, 3) <= 5 && (x+y+CURRENT_TICK/CREEP_SPREAD_PERIOD)%3 == 0 {
+			if !isInDistance && curTile.creep && g.getNumberOfCreepInRect(x-1, y-1, 3, 3) <= 5 && (x+y+CURRENT_TICK/CREEP_SPREAD_PERIOD)%CREEP_DISAPPEAR_PROBABILITY == 0 {
 				removeCreepTiles = append(removeCreepTiles, curTile)
 			}
 		}
